875a599eb270e148edb282640602817d9dbc6d10
lokeshverma1109 <lokeshverma1109@gmail.com>
2025-03-25 15:57:32 +0000

    variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 12344445
+let accountEmail="xyz@google.com"
+accountPassword ="1254678"
+accountcity = "bhopal"
+let accountState;
+
+accountEmail ="h@dgjd.com"
+accountPassword="526466"
+accountcity="indore"
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState]);
+
